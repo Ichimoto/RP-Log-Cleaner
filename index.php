4a00318833a8855f7e4ce0708dd1623e4c83dc07
Zachary Powell <zacthespack@gmail.com>
2016-03-17 11:28:44 +0000

    Updated to V0.4
    <ul>
      <li>Added support to clear GM Meter</li>
      <li>Added support to clear CCS</li>
      <li>Added Bootstrap CSS</li>
      <li>Support for post data from another location i.e inworld HUD</li>
      <li>Moved PHP to its own file</li>
    </ul>

--- index.php
+++ index.php
@@ -1,174 +1,71 @@
 <?php
-//remove the timestamp from a line for parsing
-function stripTimeStamp($line){
-	$newline = "";
-  	$arr1 = str_split($line);
-	$foundtimestamp = false;
-	$foundspace = false;
-	$foundchat = false;
-	for ($x = 0; $x <= count($arr1); $x++) {
-	    if ($arr1[$x] === '[' && !$foundtimestamp) $foundtimestamp = true;
-	    
-		if ($foundtimestamp && $arr1[$x] === ' ') $foundspace = true;
-		
-		if ($foundspace && $arr1[$x] !== ' ') $foundchat = true;
-		
-		if ($foundchat) $newline = $newline . $arr1[$x];
-	} 
-	if (!$foundtimestamp){
-		return false;
-	}else{
-		return $newline;
-	}
-}
-
-//Form submitted
-if(isset($_POST['submit'])) {
-  //Error checking
-  if(!$_POST['text']) {
-    $error['error'] = "<p>Please supply your RP Text.</p>\n";
-  }
-
-  //No errors, process
-  if(!is_array($error)) {
-	  $text = $_POST['text'];
-		
-	  $linegap = $_POST['line'];
-	  $dcs = $_POST['dcs'];
-	  $ooc = $_POST['ooc'];
-	  $online = $_POST['online']; 
-	  $notimestamp = $_POST['notimestamp']; 
-	  $simenter = $_POST['simenter'];
-	  $landstream = $_POST['landstream'];
-
-	  if ($notimestamp == 'Yes') {
-		  $editedtext = "";
-		  foreach(preg_split("/((\r?\n)|(\r\n?))/", $text) as $line){
-		  		$line2 = stripTimeStamp($line);
-		 		if (!($line2 === false)) {
-		 			$editedtext = $editedtext . $line . "\r\n";
-		 		}
-		  }
-		  $text = $editedtext;
-	  }
-	  if ($dcs == 'Yes') {
-		  $editedtext = "";
-		  
-		  foreach(preg_split("/((\r?\n)|(\r\n?))/", $text) as $line){
-			  	$line2 = stripTimeStamp($line);
-			 	if (!(substr( $line2, 0, 3 ) === "DCS")) {
-			 		$editedtext = $editedtext . $line . "\r\n";
-			 	}  
-		  }
-		  $text = $editedtext;
-	  }
-	  
-	  if ($ooc == 'Yes') {
-		  $editedtext = "";
-		  
-		  foreach(preg_split("/((\r?\n)|(\r\n?))/", $text) as $line){
-			  	$line2 = stripTimeStamp($line);
-			 	if (!(substr( $line2, 0, 2 ) === "((") && !(substr( $line2, -2 ) === "))")) {
-			 		$editedtext = $editedtext . $line . "\r\n";
-			 	}  
-		  }
-		  $text = $editedtext;
-	  }
-	  
-	  if ($online == 'Yes') {
-		  $editedtext = "";
-		  
-		  foreach(preg_split("/((\r?\n)|(\r\n?))/", $text) as $line){
-			    $linecheck = preg_replace("/[^A-Za-z0-9 ]/", '', $line);
-			  	$check = substr($linecheck, -6);
-			 	 
-				if ($check !== "ffline" && $check !== "online"){
-					$editedtext = $editedtext . $line . "\r\n";
-				}
-		  }
-		  $text = $editedtext;
-	  }
-	  
-	  if ($simenter == 'Yes') {
-		  $editedtext = "";
-		  
-		  foreach(preg_split("/((\r?\n)|(\r\n?))/", $text) as $line){
-			  	$line2 = stripTimeStamp($line);
-			 	if (strpos($line2, 'left the region') === false && strpos($line2, 'entered the region') === false) {
-			 		$editedtext = $editedtext . $line . "\r\n";
-			 	}  
-		  }
-		  $text = $editedtext;
-	  }
-	  
-	  if ($landstream == 'Yes'){
-		  $editedtext = "";
-		  
-		  foreach(preg_split("/((\r?\n)|(\r\n?))/", $text) as $line){
-			  	$line2 = stripTimeStamp($line);
-			 	if (!(substr( $line2, 0, 11 ) === "Now playing")) {
-			 		$editedtext = $editedtext . $line . "\r\n";
-			 	}  
-		  }
-		  $text = $editedtext;
-	  }
-	  	  
-	  if ($linegap == 'Yes') {
-		  $editedtext = "";
-		  
-		  foreach(preg_split("/((\r?\n)|(\r\n?))/", $text) as $line){
-			  $editedtext = $editedtext . $line . "\r\n" ."\r\n";
-		  } 
-		  
-		  $text = $editedtext;
-	  }
-  }
-}
+include 'functions.php';
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-
-        "http://www.w3.org/TR/html4/loose.dtd">
-
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8">
 	<title>RP Log Cleaner</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
 <body>
 	
-	<a href="https://github.com/zacthespack/RP-Log-Cleaner"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
+	<style>#forkongithub a{background:#000;color:#fff;text-decoration:none;font-family:arial,sans-serif;text-align:center;font-weight:bold;padding:5px 40px;font-size:1rem;line-height:2rem;position:relative;transition:0.5s;}#forkongithub a:hover{background:#c11;color:#fff;}#forkongithub a::before,#forkongithub a::after{content:"";width:100%;display:block;position:absolute;top:1px;left:0;height:1px;background:#fff;}#forkongithub a::after{bottom:1px;top:auto;}@media screen and (min-width:800px){#forkongithub{position:fixed;display:block;top:0;right:0;width:200px;overflow:hidden;height:200px;z-index:9999;}#forkongithub a{width:200px;position:absolute;top:40px;right:-60px;transform:rotate(45deg);-webkit-transform:rotate(45deg);-ms-transform:rotate(45deg);-moz-transform:rotate(45deg);-o-transform:rotate(45deg);box-shadow:4px 4px 10px rgba(0,0,0,0.8);}}</style><span id="forkongithub"><a href="https://github.com/zacthespack/RP-Log-Cleaner">Fork me on GitHub</a></span>
 	
-	ALPHA VERSION <a href="/changelog.html">0.3</a><br>
+	<div class="container">
+		<h1>RP Log Cleaner</h1>
+		<h2>ALPHA VERSION <a href="/changelog.html">0.4</a><br></h2>
 
-	<p>Born out of the laziness of one Denenthorn Masukami this tool is designed to quickly and easily clean Role Play Logs,<br>designed for use on the Second Life sim Toxian City but slowly developing to become a SL wide clener working with a range of combact systems</p>
-	<form method="post">
- 	   <?=$error['error']?>
-	   Combact System Used:<select name="cs">
-	     <option value="dcs">DCS2</option>
-	   </select>
-  	 	<p><label for="text">RP Text to Clean:<br></label><textarea type="text" id="text" name="text"><?=$text?></textarea></p>
-  		<input type="checkbox" name="line" value="Yes" checked> Line Break Posts
-  	  	<input type="checkbox" name="dcs" value="Yes" checked> Filter Combact System spam
-  	  	<input type="checkbox" name="ooc" value="Yes" checked> Remove OOC posts
-  	  	<input type="checkbox" name="online" value="Yes" checked> Remove Online/Offline posts
-  	  	<input type="checkbox" name="notimestamp" value="Yes" checked> Remove lines with no timestamp
-  	  	<input type="checkbox" name="simenter" value="Yes" checked> Remove sim leave/enter messages
-  	  	<input type="checkbox" name="landstream" value="Yes" checked> Remove Land Stream messages
-  	  	<p><input type="submit" name="submit" value="Clean Text" /></p>
-	</form>
+		<p>Born out of the laziness of one Denenthorn Masukami this tool is designed to quickly and easily clean Role Play Logs,<br>designed for use on the Second Life sim Toxian City but slowly developing to become a SL wide clener working with a range of combact systems</p>
+		<form method="post">
+			<?=$error['error']?>
+			Combact System Used:<select name="cs">
+				<option value="DCS">DCS2</option>
+				<option value="GM">GM Meter</option>
+				<option value="CCS">CCS</option>
+			</select>
+			<p><label for="text">RP Text to Clean:<br></label><textarea class="form-control" type="text" id="text" name="text" rows="10"><?=$text?></textarea></p>
+			<div class="row">
+			  <div class="col-md-4">
+				  <input type="checkbox" name="line" value="Yes" checked> Line Break Posts<br>
+				  <input type="checkbox" name="dcs" value="Yes" checked> Filter Combact System spam<br>
+				  <input type="checkbox" name="ooc" value="Yes" checked> Remove OOC posts<br>
+			  </div>
+			  <div class="col-md-4">
+				  <input type="checkbox" name="notimestamp" value="Yes" checked> Remove lines with no timestamp<br>
+				  <input type="checkbox" name="simenter" value="Yes" checked> Remove sim leave/enter messages<br>
+				  <input type="checkbox" name="landstream" value="Yes" checked> Remove Land Stream messages<br>
+			  </div>
+			  <div class="col-md-4">
+				  <input type="checkbox" name="online" value="Yes" checked> Remove Online/Offline posts<br>
+				  
+			  </div>
+			</div>
+			
+			<p><input type="submit" name="submit" value="Clean Text" /></p>
+		</form>
 
-	<p>Built by Lyrza (zp.lyric) if you find a bug or have a feature request please email: <a href="mailto:support@rplogcleaner.com" target="_top">support@rplogcleaner.com</a>
-</p>
+		<p>Built by Lyrza (zp.lyric) if you find a bug or have a feature request please email: <a href="mailto:support@rplogcleaner.com" target="_top">support@rplogcleaner.com</a>
+		</div>
+	
+	</p>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-75168411-1', 'auto');
-  ga('send', 'pageview');
+	ga('create', 'UA-75168411-1', 'auto');
+	ga('send', 'pageview');
 
-</script>
+	</script>
 </body>
 </html>
